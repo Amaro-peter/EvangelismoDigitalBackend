b78f381c7efab02267ab35a866356322836444d6
Alan Gomes <alan.gomes7email@gmail.com>
2026-01-26 21:27:51 -0300

    create-church-use-case

--- src/use-cases/churches/create-church-use-case.ts
+++ src/use-cases/churches/create-church-use-case.ts
@@ -1,5 +1,8 @@
+import { Prisma } from '@prisma/client'
 import { ChurchesRepository } from '@repositories/churches-repository'
+import { ChurchAlreadyExistsError } from '@use-cases/errors/church-already-exists-error'
 import { CreateChurchError } from '@use-cases/errors/create-church-error'
+import { NoAddressError } from '@use-cases/errors/no-address-error'
 
 interface CreateChurchUseCaseRequest {
   name: string
@@ -12,7 +15,7 @@ interface CreateChurchUseCaseResponse {
   id: number
   publicId: string
   name: string
-  address: string | null
+  address: string
   lat: number
   lon: number
   geog?: unknown | null
@@ -24,17 +27,49 @@ export class CreateChurchUseCase {
   constructor(private churchesRepository: ChurchesRepository) {}
 
   async execute({ name, address, lat, lon }: CreateChurchUseCaseRequest): Promise<CreateChurchUseCaseResponse> {
-    const church = await this.churchesRepository.createChurch({
+    if (!address || address.trim() === '') {
+      throw new NoAddressError()
+    }
+
+    const churchWithSameName = await this.churchesRepository.findByName(name)
+
+    if (churchWithSameName !== null) {
+      throw new ChurchAlreadyExistsError()
+    }
+
+    const churchAlreadyExists = await this.churchesRepository.findByParams({
       name,
-      address,
       lat,
       lon,
     })
 
-    if (!church) {
-      throw new CreateChurchError()
+    if (churchAlreadyExists !== null) {
+      throw new ChurchAlreadyExistsError()
     }
 
-    return church
+    try {
+      const church = await this.churchesRepository.createChurch({
+        name,
+        address,
+        lat,
+        lon,
+      })
+
+      if (!church) {
+        throw new CreateChurchError()
+      }
+
+      return church
+    } catch (err: any) {
+      if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
+        throw new ChurchAlreadyExistsError()
+      }
+
+      if (err.message === 'church-already-exists') {
+        throw new ChurchAlreadyExistsError()
+      }
+
+      throw err
+    }
   }
 }
